10a2e1ee831ab4b314b31add9552684973e1f836
jbrad88 <jodybradley@gmail.com>
2020-02-21 17:33:39 +0000

    Added labs

---
+++ labs/Topic03-variables/lab03.01-hello.py
@@ -0,0 +1,5 @@
+# Program that reads in a persons name and
+# outputs hello thatName
+
+name = input('Enter your name:')
+print('Hello', name)
---
+++ labs/Topic03-variables/lab03.2-sub.py
@@ -0,0 +1,12 @@
+# Program to subtact one number from another.
+# Input reads in a string so we need to conver it
+# into an integer so we can perform mathematical operations.
+# Do this with "int".
+
+numberone = int(input('Enter first number:'))
+
+numbertwo = int(input('Enter second number:'))
+
+answer = numberone - numbertwo
+
+print(numberone, 'minus', numbertwo, 'is', answer)
