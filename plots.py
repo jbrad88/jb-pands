57fa2a9e65134b0990dd61db60d5f4da1da29766
jbrad88 <jodybradley@gmail.com>
2020-03-23 15:49:52 +0000

    Task week 8

---
+++ plots.py
@@ -0,0 +1,17 @@
+# Jody Bradley
+# This program displays a plot of the following functions:
+# 1. f(x) = x
+# 2. g(x) = x**2
+# 3. h(x) = x**3
+# ... in the range of [0,4]
+
+def f(x):
+    return x
+
+def g(x):
+    return x * x
+
+def h(x):
+    x ** 3
+
+
